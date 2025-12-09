d8151c729d9f35b6c3c9a6908f272ddafda8c213
ThreeTNT <lenazhu2007@gmail.com>
2025-12-09 14:09:48 -0500

    try to make my own workflow

---
+++ .github/workflows/helper.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+install_deps_linux() {
+    sudo apt-get update && sudo apt-get install -y \
+        libgl1-mesa-dev \
+        libwayland-dev \
+        libxinerama-dev \
+        libxcursor-dev \
+        libxkbcommon-dev \
+        libxrandr-dev \
+        libxi-dev \
+        ninja-build
+}
+
+install_deps_windows() {
+    true
+}
+
+install_deps_macos() {
+    brew install ninja
+}
+
+install_deps() {
+    if [[ $RUNNER_OS == "Linux" ]]; then
+        install_deps_linux
+    elif [[ $RUNNER_OS == "Windows" ]]; then
+        install_deps_windows
+    elif [[ $RUNNER_OS == "macOS" ]]; then
+        install_deps_macos
+    fi
+}
+
+prepare_build() {
+    mkdir -p mujoco/build
+    cd mujoco/build &&
+    cmake .. \
+        -DCMAKE_BUILD_TYPE:STRING=Release \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION:BOOL=ON \
+        -DMUJOCO_BUILD_EXAMPLES:BOOL=OFF
+}
+
+build_mujoco() {
+    cd mujoco/build && cmake --build . --config=Release
+}
+
+build_wheel_and_sdist() {
+    cd mujoco/python &&
+    uv venv .venv &&
+    if [[ "$RUNNER_OS" == "Windows" ]]; then
+        source .venv/Scripts/activate
+    else
+        source .venv/bin/activate
+    fi &&
+    uv pip install pip &&
+    bash make_sdist.sh
+}
+
+VALID_FUNCTIONS=()
+while IFS= read -r func_name; do
+  VALID_FUNCTIONS+=("$func_name")
+done < <(grep -E '^[[:alnum:]_]+\(\)' "$0" | sed 's/().*$//')
+
+if [[ ! " ${VALID_FUNCTIONS[*]} " =~ " ${1} " ]]; then
+    echo "Usage: $0 {$(IFS='|'; echo "${VALID_FUNCTIONS[*]}")}, got '$1'"
+    exit 1
+fi
+
+set -xe
+
+"$1"
